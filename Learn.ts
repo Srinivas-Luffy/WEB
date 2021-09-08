e6c3f15bbc410ce8e11c5f329d32b71f3f15acdb
Srinivas-Luffy <srinivas.v09@students.vnit.ac.in>
2021-09-08 09:03:38 +0530

    Web

---
+++ Learn.ts
@@ -0,0 +1,13 @@
+class myClass
+{
+    j: number;
+    constructor(k: number)
+    {
+         this.j = k;
+    } 
+value(){
+    return("The number is "+this.j);
+    } 
+}
+let myobj = new myClass(6);
+console.log(myobj.j);
